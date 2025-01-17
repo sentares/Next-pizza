336ad7f4d416b6588802655fe3926e287f3111a7
sentares <aydararzybekov2002@mail.ru>
2025-01-17 15:33:01 +0600

    create post request

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -6,3 +6,10 @@ export async function GET() {
 
 	return NextResponse.json(users)
 }
+
+export async function POST(request: Request) {
+	const body = await request.json()
+	const user = await prisma.user.create({ data: body })
+
+	return NextResponse.json(user)
+}
