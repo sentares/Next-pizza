fdec66975d6d61e10094dc24fac2636d849caa71
Aidar <aydararzybekov2002@mail.ru>
2025-01-19 02:02:06 +0600

    create search and add api request

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { cn } from '@/lib/utils'
 import { Container } from './container'
 import Image from 'next/image'
+import Link from 'next/link'
+import { SearchInput } from './search-input'
 
 interface Props {
 	className?: string
@@ -12,21 +14,24 @@ export const Header: React.FC<Props> = ({ className }) => {
 		<header className={cn('border border-b', className)}>
 			<Container className='flex items-center justify-between p-8'>
 				{/* Rignt */}
-				<div className='flex items-center gap-4'>
-					<Image src='/logo.png' alt='Logo' width={35} height={35} />
-					<div>
-						<h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
-						<p className='text-sm text-gray-400 leading-3'>
-							вкусней уже некуда
-						</p>
-					</div>
-				</div>
 
-				{/* {hasSearch && (
-					<div className='mx-10 flex-1'>
-						<SearchInput />
+				<Link href='/'>
+					<div className='flex items-center gap-4'>
+						<Image src='/logo.png' alt='Logo' width={35} height={35} />
+						<div>
+							<h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
+							<p className='text-sm text-gray-400 leading-3'>
+								вкусней уже некуда
+							</p>
+						</div>
 					</div>
-				)} */}
+				</Link>
+
+				{/* {hasSearch && ( */}
+				<div className='mx-10 flex-1'>
+					<SearchInput />
+				</div>
+				{/* )} */}
 
 				{/* Правая часть */}
 				<div className='flex items-center gap-3'>
