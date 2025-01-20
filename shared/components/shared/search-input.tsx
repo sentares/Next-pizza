181ca5eaf16cc56c2d84fb35af55542e77cad838
Aidar <aydararzybekov2002@mail.ru>
2025-01-20 16:47:10 +0600

    update file structure

--- shared/components/shared/search-input.tsx
+++ shared/components/shared/search-input.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { cn } from '@/shared/lib/utils'
 import { Api } from '@/app/services/api-client'
+import { cn } from '@/shared/lib/utils'
 import { Product } from '@prisma/client'
 import { Search } from 'lucide-react'
 import Link from 'next/link'
--- shared/services/instance.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL,
-	headers: {
-		'Content-Type': 'application/json',
-	},
-})
