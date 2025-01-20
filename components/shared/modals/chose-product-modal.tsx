4c9fc9a1769e8089836499047378472a1722e94b
Aidar <aydararzybekov2002@mail.ru>
2025-01-20 12:04:49 +0600

    create modal dialog

---
+++ components/shared/modals/chose-product-modal.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog'
+import { cn } from '@/lib/utils'
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import { Product } from '@prisma/client'
+
+interface Props {
+	product: Product
+	className?: string
+}
+
+export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
+	const router = useRouter()
+
+	return (
+		<Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
+			<DialogContent
+				className={cn(
+					'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden',
+					className
+				)}
+			>
+				<DialogTitle>{product.name}</DialogTitle>
+			</DialogContent>
+		</Dialog>
+	)
+}
