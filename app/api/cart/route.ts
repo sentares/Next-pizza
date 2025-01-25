88a7c873201650125a38d6d19b1260793145d51a
Aidar <aydararzybekov2002@mail.ru>
2025-01-25 14:55:42 +0600

    update cockies token

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
 	try {
-		const token = req.cookies.get('token')?.value
+		const token = req.cookies.get('cartToken')?.value
 
 		if (!token) {
 			return NextResponse.json({ totalAmount: 0, items: [] })
