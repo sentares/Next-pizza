d0969e4f02e5291d58347dfe206d528171a42428
Aidar <aydararzybekov2002@mail.ru>
2025-01-28 22:27:08 +0600

    test

--- shared/components/shared/cart-button.tsx
+++ shared/components/shared/cart-button.tsx
@@ -5,31 +5,26 @@ import React from 'react'
 import { Button } from '../ui'
 import { ArrowRight, ShoppingCart } from 'lucide-react'
 import { CartDrawer } from './cart-drawer'
-// import { CartDrawer } from './cart-drawer'
-// import { useCartStore } from '@/shared/store'
+import { useCartStore } from '@/app/store'
 
 interface Props {
 	className?: string
 }
 
 export const CartButton: React.FC<Props> = ({ className }) => {
-	// const [totalAmount, items, loading] = useCartStore(state => [
-	// 	state.totalAmount,
-	// 	state.items,
-	// 	state.loading,
-	// ])
+	const { items, totalAmount, loading } = useCartStore()
 
 	return (
 		<CartDrawer>
 			<Button
-			// loading={loading}
-			// className={cn('group relative', { 'w-[105px]': loading }, className)}
+				loading={loading}
+				className={cn('group relative', { 'w-[105px]': loading }, className)}
 			>
-				{/* <b>{totalAmount} ₽</b> */}
+				<b>{totalAmount} ₽</b>
 				<span className='h-full w-[1px] bg-white/30 mx-3' />
 				<div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
 					<ShoppingCart size={16} className='relative' strokeWidth={2} />
-					{/* <b>{items.length}</b> */}
+					<b>{items.length}</b>
 				</div>
 				<ArrowRight
 					size={20}
